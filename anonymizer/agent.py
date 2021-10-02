cd9868a9c457728f42fa89027bae6acb1cd6b3ff
Wiktor Łazarski <wjlazarski@gmail.com>
2021-10-02 20:06:00 +0200

    Fix issue with '-'.

--- anonymizer/agent.py
+++ anonymizer/agent.py
@@ -21,8 +21,20 @@ class DocumentAnonymizer:
         people_anonymization = peo_anon.anonymize_people(nlp_results)
         place_anonymization = pla_anon.anonymize_places(nlp_results)
 
-        return [*people_anonymization, *place_anonymization]
+        return self._postprocess([*people_anonymization, *place_anonymization])
 
     def _preprocess(self, text: str) -> str:
         text = text.strip()
         return text
+
+    def _postprocess(self, raw_outputs: t.List[AnonymizationResult]) -> t.List[AnonymizationResult]:
+        postprocessed = [
+            AnonymizationResult(
+                entity=output.entity.replace(" - ", "-"),
+                anonymization=output.anonymization,
+                anon_type=output.anon_type,
+            )
+            for output in raw_outputs
+        ]
+
+        return postprocessed
