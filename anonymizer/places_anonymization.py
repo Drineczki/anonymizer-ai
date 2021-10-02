e31cbf63b46a35b937a3efb61d5c04f64620b11f
Wiktor Łazarski <wjlazarski@gmail.com>
2021-10-02 18:52:38 +0200

    More places anonymization.

--- anonymizer/places_anonymization.py
+++ anonymizer/places_anonymization.py
@@ -20,42 +20,10 @@ PUBLIC_PLACES_ENTITIES: t.List[str] = __load_assets(c.PUBLIC_ENTITIES_YAML)
 def anonymize_places(nlp_results: t.Iterable[spacy.tokens.Token]) -> t.List[AnonymizationResult]:
     city_tokens = __preprocess_cities(nlp_results)
     city_anonymizations = __anonymize_cities(city_tokens)
+    geoloc_tokens = __preprocess_geoloc(nlp_results)
+    geoloc_anonymizations = __anonymize_geoloc(geoloc_tokens)
 
-    return city_anonymizations
-
-
-def __anonymize_cities(city_tokens: t.List[spacy.tokens.Token]) -> t.List[AnonymizationResult]:
-    merged_city_tokens: t.List[t.List[spacy.tokens.Token, ...]] = []
-    loop_cnt = 0
-    while loop_cnt < len(city_tokens):
-        if city_tokens[loop_cnt].ent_iob_ == "B":
-            full_entity = [city_tokens[loop_cnt]]
-            loop_cnt += 1
-            if loop_cnt == len(city_tokens):
-                break
-
-            while city_tokens[loop_cnt].ent_iob_ == "I":
-                full_entity.append(city_tokens[loop_cnt])
-                loop_cnt += 1
-
-                if loop_cnt == len(city_tokens):
-                    break
-
-            merged_city_tokens.append(full_entity)
-            continue
-
-        loop_cnt += 1
-
-    anonymization_results: t.List[AnonymizationResult] = []
-    for city_tokens in merged_city_tokens:
-        city_text = " ".join([token.text for token in city_tokens])
-
-        anonymization = f"{city_tokens[0].text[0]}."
-        res = AnonymizationResult(entity=city_text, anonymization=anonymization, anon_type="place")
-
-        anonymization_results.append(res)
-
-    return anonymization_results
+    return [*city_anonymizations, *geoloc_anonymizations]
 
 
 def __preprocess_cities(nlp_results: t.Iterable[spacy.tokens.Token]) -> t.List[spacy.tokens.Token]:
@@ -88,3 +56,115 @@ def __preprocess_cities(nlp_results: t.Iterable[spacy.tokens.Token]) -> t.List[s
         loop_cnt += 1
 
     return city_tokens
+
+
+def __anonymize_cities(city_tokens: t.List[spacy.tokens.Token]) -> t.List[AnonymizationResult]:
+    merged_city_tokens: t.List[t.List[spacy.tokens.Token, ...]] = []
+    loop_cnt = 0
+    while loop_cnt < len(city_tokens):
+        if city_tokens[loop_cnt].ent_iob_ == "B":
+            full_entity = [city_tokens[loop_cnt]]
+            loop_cnt += 1
+            if loop_cnt == len(city_tokens):
+                break
+
+            while city_tokens[loop_cnt].ent_iob_ == "I":
+                full_entity.append(city_tokens[loop_cnt])
+                loop_cnt += 1
+
+                if loop_cnt == len(city_tokens):
+                    break
+
+            merged_city_tokens.append(full_entity)
+            continue
+
+        loop_cnt += 1
+
+    anonymization_results: t.List[AnonymizationResult] = []
+    for tokens in merged_city_tokens:
+        city_text = " ".join([token.text for token in tokens])
+
+        anonymization = f"{tokens[0].text[0]}."
+        lemmas = set([token.lemma_ for token in tokens])
+        region_lemmas = set(["gmina", "powiat", "wojewodztwie"])
+        if len(region_lemmas.intersection(lemmas)) != 0:
+            geoloc_name = tokens[0].text
+            anonymization = f"{geoloc_name} (...)"
+
+        res = AnonymizationResult(entity=city_text, anonymization=anonymization, anon_type="place")
+
+        anonymization_results.append(res)
+
+    return anonymization_results
+
+
+def __preprocess_geoloc(nlp_results: t.Iterable[spacy.tokens.Token]) -> t.List[spacy.tokens.Token]:
+    return [token for token in nlp_results if token.ent_type_ == "geogName"]
+
+
+def __anonymize_geoloc(geoloc_tokens: t.List[spacy.tokens.Token]) -> t.List[AnonymizationResult]:
+    merged_geoloc_tokens: t.List[t.List[spacy.tokens.Token, ...]] = []
+    loop_cnt = 0
+    while loop_cnt < len(geoloc_tokens):
+        if geoloc_tokens[loop_cnt].ent_iob_ == "B":
+            full_entity = [geoloc_tokens[loop_cnt]]
+            loop_cnt += 1
+            if loop_cnt == len(geoloc_tokens):
+                break
+
+            while geoloc_tokens[loop_cnt].ent_iob_ == "I":
+                full_entity.append(geoloc_tokens[loop_cnt])
+                loop_cnt += 1
+
+                if loop_cnt == len(geoloc_tokens):
+                    break
+
+            if loop_cnt == len(geoloc_tokens):
+                merged_geoloc_tokens.append(full_entity)
+                break
+
+            # ul. case
+            if geoloc_tokens[loop_cnt].ent_iob_ == "B" and geoloc_tokens[loop_cnt - 1].text == ".":
+                full_entity.append(geoloc_tokens[loop_cnt])
+                loop_cnt += 1
+                if loop_cnt == len(geoloc_tokens):
+                    break
+
+                while geoloc_tokens[loop_cnt].ent_iob_ == "I":
+                    full_entity.append(geoloc_tokens[loop_cnt])
+                    loop_cnt += 1
+
+                    if loop_cnt == len(geoloc_tokens):
+                        break
+
+            merged_geoloc_tokens.append(full_entity)
+            continue
+
+        loop_cnt += 1
+
+    anonymization_results: t.List[AnonymizationResult] = []
+    geoloc_lemmas = set(["plac", "skwer", "park", "rondo"])
+    for tokens in merged_geoloc_tokens:
+        geoloc_text = " ".join([token.text for token in tokens])
+
+        anonymization = ""
+        lemmas = set([token.lemma_ for token in tokens])
+        if "ul" in [token.text for token in tokens]:
+            anonymization = "ul. (...)"
+        elif len(geoloc_lemmas.intersection(lemmas)) != 0:
+            geoloc_name = tokens[0].text
+            anonymization = f"{geoloc_name} (...)"
+        else:
+            for token in tokens:
+                if token.text[0].isupper():
+                    anonymization = f"{anonymization} {token.text[0]}."
+                    break
+                anonymization = f"{anonymization} {token.text}"
+
+        res = AnonymizationResult(
+            entity=geoloc_text, anonymization=anonymization, anon_type="geoloc"
+        )
+
+        anonymization_results.append(res)
+
+    return anonymization_results
